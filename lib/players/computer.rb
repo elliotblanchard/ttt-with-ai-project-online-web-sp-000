2a32c88bd9f37b3de178ab9d57153992c8d2fb95
Elliot Blanchard <public@elliotblanchard.com>
2020-04-25 22:14:20 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,8 +5,18 @@ module Players
     def move(board)
       #random for now
       #binding.pry
-      my_move = rand(8)+1
-      my_move.to_s
+      #my_move = rand(8)+1
+      #my_move.to_s
+
+      #Can I make a move that will win the game?
+
+      #Is the opponent about to win? Block!
+
+      #If a corner space is free, take it
+
+      #If the center is free, take it
+
+      #Otherwise, take a side space
     end
   end
 end

209523b16b83ae76d082cf615a106c4dbc32f3b4
Elliot Blanchard <public@elliotblanchard.com>
2020-04-26 03:14:02 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -81,6 +81,7 @@ class Board
       if occupied_x == 2 || occupied_o == 2
         target_index = empty_index
         puts "Empty index: #{empty_index} and Target index: #{target_index}"
+        binding.pry
       end
     end
     target_index

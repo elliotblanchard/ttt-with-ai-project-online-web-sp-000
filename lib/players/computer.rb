80d3fa9e11144ee3ccaa084f63d691006b6c4137
Elliot Blanchard <public@elliotblanchard.com>
2020-04-26 02:32:50 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,23 +5,23 @@ module Players
     CORNER_COMBINATIONS = [0,2,6,8]
     SIDE_COMBINATIONS = [1,3,5,7]
 
-    def corner_free?
+    def corner_free?(board)
       CORNER_COMBINATIONS.any? do |index|
         if (board.cells[index] != "X") && (board.cells[index] != "O")
           return index
         end
       end
     end
-    
+
     def move(board)
       #Can I make a move that will win the game?
       #Is the opponent about to win? Block!
       if board.near_win? != nil
         my_move = board.near_win?.to_i+1
-      elsif corner_free?
+      elsif corner_free?(board)
         #If a corner space is free, take it
         puts "Corner free"
-        my_move = corner_free?+1
+        my_move = corner_free?(board)+1
       elsif (board.cells[4] != "X") && (board.cells[4] != "O")
         #If the center is free, take it
         my_move = 5

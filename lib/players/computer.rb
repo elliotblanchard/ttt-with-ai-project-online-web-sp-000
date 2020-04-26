e1b2d0b065e3604dd16272f66fa6f024adc327bd
Elliot Blanchard <public@elliotblanchard.com>
2020-04-26 02:29:32 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,22 +3,33 @@ require 'pry'
 module Players
   class Computer < Player
     def move(board)
-      #random for now
-      #binding.pry
+      CORNER_COMBINATIONS = [0,2,6,8]
+      SIDE_COMBINATIONS = [1,3,5,7]
 
       #Can I make a move that will win the game?
       #Is the opponent about to win? Block!
       if board.near_win? != nil
         my_move = board.near_win?.to_i+1
+      elsif corner_free?
+        #If a corner space is free, take it
+      elseif (board.cells[4] != "X") && (board.cells[4] != "O")
+        #If the center is free, take it
+        my_move = 5
       else
-        my_move = rand(8)+1
+        #Otherwise, take a side space
+        my_move = board.side_free?
       end
-      #If a corner space is free, take it
 
-      #If the center is free, take it
-
-      #Otherwise, take a side space
+      #my_move = rand(8)+1
       my_move.to_s
+    end
+  end
+
+  def corner_free?
+    CORNER_COMBINATIONS.any? do |index|
+      if (board.cells[index] != "X") && (board.cells[index] != "O")  
+        return index
+      end
     end
   end
 end
